a02225ebd31bb331b39cb80ae18c73ea7076a9c0
jose <josewcab.fp@gmail.com>
2020-02-27 19:35:29 +0100

    add loader

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,35 @@
 console.log("js cargado!");
+
+
+
+
+
+//Set your href attribute as href="javascript:delay('URL')" and JavaScript:
+
+function delay (URL) {
+    setTimeout( function() { window.location = URL }, 2000 );
+}
+
+
+
+
+//If you want to delay every link on your page, you can do it with jQuery like this
+
+// $(function(){
+//     $("a").click(function(evt){
+//         var link = $(this).attr("href");
+//         setTimeout(function() {
+//             window.location.href = link;
+//         }, 500);
+//     });
+// });
+
+// var dismiss = window.addEventListener('load', function(){
+//     document.getElementById("pageloader").classList.add("dimissloader");
+//     this.console.log("hola");
+// });
+
+setTimeout(() => {
+    document.getElementById("pageloader").classList.add("dimissloader");
+    console.log("holamundo");
+}, 1000);
