339e2f11c722e7870fca3d6dfd7a4dad0d0e1195
Mariano <Mariano@MacBook-Ma.local>
2021-01-25 09:57:40 +0100

    Tema 9

--- scripts_arduino/librerias/servo.h
+++ scripts_arduino/librerias/servo.h
@@ -1,17 +1,14 @@
 /*
   Servo.h - Interrupt driven Servo library for Arduino using 16 bit timers- Version 2
   Copyright (c) 2009 Michael Margolis.  All right reserved.
-
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
-
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
-
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -22,17 +19,13 @@
   the desired pin to the attach() method.
   The servos are pulsed in the background using the value most recently 
   written using the write() method.
-
   Note that analogWrite of PWM on pins associated with the timer are 
   disabled when the first servo is attached.
   Timers are seized as needed in groups of 12 servos - 24 servos use two 
   timers, 48 servos will use four.
   The sequence used to sieze timers is defined in timers.h
-
   The methods are:
-
     Servo - Class for manipulating servo motors connected to Arduino pins.
-
     attach(pin )  - Attaches a servo motor to an i/o pin.
     attach(pin, min, max  ) - Attaches to a pin setting min and max values in microseconds
     default min is 544, max is 2400  
@@ -65,8 +58,16 @@
 #include "sam/ServoTimers.h"
 #elif defined(ARDUINO_ARCH_SAMD)
 #include "samd/ServoTimers.h"
+#elif defined(ARDUINO_ARCH_STM32F4)
+#include "stm32f4/ServoTimers.h"
+#elif defined(ARDUINO_ARCH_NRF52)
+#include "nrf52/ServoTimers.h"
+#elif defined(ARDUINO_ARCH_MEGAAVR)
+#include "megaavr/ServoTimers.h"
+#elif defined(ARDUINO_ARCH_MBED)
+#include "mbed/ServoTimers.h"
 #else
-#error "This library only supports boards with an AVR, SAM or SAMD processor."
+#error "This library only supports boards with an AVR, SAM, SAMD, NRF52 or STM32F4 processor."
 #endif
 
 #define Servo_VERSION           2     // software version of this library
@@ -80,6 +81,8 @@
 #define MAX_SERVOS   (_Nbr_16timers  * SERVOS_PER_TIMER)
 
 #define INVALID_SERVO         255     // flag indicating an invalid servo index
+
+#if !defined(ARDUINO_ARCH_STM32F4)
 
 typedef struct  {
   uint8_t nbr        :6 ;             // a pin number from 0 to 63
@@ -109,4 +112,5 @@ private:
    int8_t max;                       // maximum is this value times 4 added to MAX_PULSE_WIDTH   
 };
 
+#endif
 #endif
--- scripts_arduino/tema9.ino
+++ scripts_arduino/tema9.ino
@@ -1,27 +1,37 @@
-//www.elegoo.com
-//2016.12.08
+// Fun Tech Academy 2020
+
+// Incluimos la librería Servo.h
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
+int pos = 0;
+// Definimos la variable de tipo Servo
+Servo servo_9;
 
-int pos = 0;    // variable to store the servo position
-
-void setup() {
-  Serial.begin(9600);
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+void setup()
+{
+   // Definimos los parámetros del servo:
+   // - pin: el número del pin al que está conectado el servo
+   // - min (opcional): el ancho de pulso, en microsegundos, correspondiente al ángulo mínimo (0 grados) en el servo (predeterminado en 544)
+   // - max (opcional): el ancho de pulso, en microsegundos, correspondiente al ángulo máximo (180 grados) en el servo (predeterminado en 2400)
+   servo_9.attach(9, 500, 2500);
 }
 
-void loop() {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
- 
+void loop()
+{
+   // barre el servo de 0 a 180 grados en pasos
+   // de 1 grados
+   for (pos = 0; pos <= 180; pos += 1) 
+   {
+      // Le decimos al servo que vaya a la posición en la variable 'pos'
+      servo_9.write(pos);
+      // Espera 100 ms para que el servo alcance la posición
+      delay(100);
+   }
+   for (pos = 180; pos >= 0; pos -= 1) 
+   {
+      // Le decimos al servo que vaya a la posición en la variable 'pos'
+      servo_9.write(pos);
+      // Espera 5 ms para que el servo alcance la posición
+      delay(5); // Wait for 5 millisecond(s)
+   }
 }
-
